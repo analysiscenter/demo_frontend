3ae74e7e0819cf40ee88319cab247c252425e6c8
Roman Kh <rhudor@gmail.com>
2017-08-29 12:04:46 +0300

    Added font-awesome

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 
+import MainPage from './MainPage.jsx'
+
 export default class App extends Component {
   render() {
     return (
      <div>
         <h1>Hello World</h1>
+        <MainPage />
       </div>);
   }
 }
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+import { Icon } from 'react-fa';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+     <div>
+        <Icon name='heartbeat' />
+      </div>);
+  }
+}
