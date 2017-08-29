a0c6d715b85ad1cab04d147b4abce5cea833e00c
Egor Illarionov <e.illarionov@analysiscenter.ru>
2017-08-29 18:11:49 +0300

    ecg template

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
 
